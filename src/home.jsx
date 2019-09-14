78603a380d2de71d68c0ea934d6b9044f0ef5666
MLC <vin_mlc@126.com>
2019-09-14 10:22:12 +0800

    重新改版

--- src/home.jsx
+++ src/home.jsx
@@ -1,44 +1,95 @@
 import React from 'react';
-import { Layout, Menu, Rate, Carousel, Modal, Descriptions} from 'antd';
+import { Layout, Menu, Rate, Carousel, Modal, Descriptions, Icon, Drawer, PageHeader, Tag, Typography} from 'antd';
 import 'antd/dist/antd.css';
 import gameico from './assets/game.png';
 import gameico2 from './assets/youxi.png';
 import shareico from './assets/fenxiang.png';
+import mlc from './assets/mlc.png';
+import logo from './assets/logo.png';
+import level0logo from './assets/fenzhijiedian.png';
+import level1logo from './assets/block.png';
 import './home.css';
+import SubMenu from 'antd/lib/menu/SubMenu';
 
 const { Header, Content, Footer } = Layout;
 
 class HomePage extends React.Component {
     state = {
-        pageindex : "home"
+        pageindex : "home",
+        menuvisible : false
     };
+
+    showMenu = ()=>{
+        this.setState(
+            {
+                menuvisible: true
+            }
+        )
+    }
+
+    hideMenu = () => {
+        this.setState({
+            menuvisible: false,
+        });
+      };
+    
 
     render() {
         return (
             <Layout className="layout">
-                <Header style={{height:'50px'}}>
-                    <div className="logo" />
+                <Header style={{height:'60px',padding:"0px 20px",display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <Icon type="menu" style={{fontSize:"30px",color:"white"}} onClick={this.showMenu}/>
+                    <div style={{display:'flex',alignItems:'center'}}>
+                    <img src={logo} className={'logo head'}/>
+                    <img src={mlc} style={{height:'50px'}}  className={'logo head'}/>
+                    </div>
+                </Header>
+                <Drawer
+                    title="目录"
+                    placement="left"
+                    closable={false}
+                    onClose={this.hideMenu}
+                    getContainer={false}
+                    visible={this.state.menuvisible}
+                    bodyStyle={{padding:'0px 0px 0px 20px'}}
+                    style={{fontWeight:'bold'}}
+                    width={200}
+                >
                     <Menu
-                        theme="dark"
-                        mode="horizontal"
+                        mode="inline"
                         defaultSelectedKeys={["home"]}
-                        style={{ lineHeight: '50px' }}
+                        defaultOpenKeys={["levels"]}
+                        style={{}}
+                        inlineIndent={1}
                         onSelect={(props) => {
                             console.log(props.key)
                             this.setState(
                                 {
-                                    pageindex: props.key
+                                    pageindex: props.key,
+                                    menuvisible: false
                                 }
                                 );
                         }}
                     >
                         <Menu.Item key="home">首页</Menu.Item>
-                        <Menu.Item key="smartdevice">智能硬件</Menu.Item>
-                        <Menu.Item key="3">联系我们</Menu.Item>
+                        <SubMenu 
+                            key="levels"
+                            title={
+                            <span>
+                                <Icon type="setting" />
+                                <span>课程内容</span>
+                            </span>}>
+                            <Menu.Item style={{marginLeft:'30px'}} key="smartdevice">智能硬件</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="3">
+                            <Icon type="mail" />
+                            联系我们
+                        </Menu.Item>
                     </Menu>
-                </Header>
-                <SubPage pageindex={this.state.pageindex} />
-                <Footer style={{textAlign: 'center' }}>Designed by Ma Lichao</Footer>
+                </Drawer>
+                <SubPage pageindex={this.state.pageindex} isMenuShown={this.state.menuvisible}>
+                </SubPage>
+                <Footer style={{textAlign: 'center',padding:'10px',fontWeight:'bold'}}>Designed by Ma Lichao</Footer>
             </Layout>
         )
     }
@@ -50,12 +101,22 @@ class HomePage extends React.Component {
 
 class SubPage extends React.Component{
     render(){
+        const menushownstlye={
+            padding: '0px 30px 0px 30px',filter:'blur(10px)',backgroundColor:'rgb(102,102,102)'
+        }
+        const menuhidestlye={
+            padding: '0px 30px 0px 30px',backgroundColor:'rgb(102,102,102)'
+        }
         if(this.props.pageindex === "home"){
             return(
-                <Content style={{ padding: '30px 30px 0px 30px' }}>
-                    <div style={{ background: '#fff', display: 'flex', height: '100%' }}>
+                <Content style={this.props.isMenuShown ? menushownstlye : menuhidestlye}>
+                    <div className={'logo label'}>
+                        <div>Scratch/Python/</div>
+                        <div>Intelligent Device</div>
+                    </div>
+                    <div style={{ background: 'rgb(102,102,102)', display: 'flex', height: '100%' }}>
                         {(() => { return <div></div> })()}
-                        <BoxContainer amount = {5}/>
+                        <BoxContainer amount={5} />
                     </div>
                 </Content>
             )
@@ -100,7 +161,10 @@ class BoxContainer extends React.Component{
     }
 
     render(){
-        let boxList = [gameico,gameico2,shareico]
+        let boxList = [1,2];
+        let boxColor = ['rgb(249,236,239)','rgb(237,197,206)','rgb(237,218,197)'];
+        //方块的内容
+        let boxContent = [EnterExam,ScratchBase];
         let modalText = () => {
             if(this.state.selectedkey === 0){
                 return(
@@ -119,7 +183,14 @@ class BoxContainer extends React.Component{
             >
                 <div style={{display:'flex',height:'15px',width:'100%'}}/>
                 {boxList.map((item, index) => {
-                    return <Box onClick={()=>{this.setState({modalVisible:true,selectedkey:index})}} key={index} icon={item} size={'200px'}/>
+                    return( 
+                        <Box 
+                            onClick={()=>{this.setState({modalVisible:true,selectedkey:index})}} 
+                            key={index}
+                            size={'265px'}
+                            backgroundColor={boxColor[index]}
+                            boxContent={boxContent[index]}/>
+                        )
                  })}
                  <div style={{display:'flex',height:'15px',width:'100%'}}/>
                  <Modal 
@@ -143,18 +214,69 @@ class Box extends React.Component{
              style={{
                 width: this.props.size,
                 height:this.props.size,
-                backgroundColor:'#FFFFFF',
+                backgroundColor:this.props.backgroundColor,
                 margin:'15px',
-                borderRadius:'5px',
-                border:'2px dashed',
-                borderColor:'rgb(214,214,214)'
+                borderRadius:'10px',
+                display:'flex',
+                justifyContent:'center',
+                alignItems:'center'
+                // border:'2px dashed',
                 }}>
-            <div className={'btn bubble'}>
-                <img style={{display:'flex',height:'100%'}} src={this.props.icon}/>
-            </div>
+                <div>
+                    {this.props.boxContent()}
+                </div>
             </div>
         )
     }
+
+    static defaultProps={
+        backgroundColor:'white'
+    }
+}
+
+//第一个box的内容
+function EnterExam(){
+        return(
+        <PageHeader
+            title="Level 0"
+            subTitle="入学测试"
+            avatar={{src: level0logo}}
+        >
+            <div style={{
+                display:'flex',
+                justifyContent:'center',
+                flexDirection:'column',
+                alignItems:'center'
+            }}>
+                <img src={shareico} style={{height:'100px'}}/>
+            </div>            
+            <Typography>
+                    做一些问卷调查，查看您的孩子适合什么等级的课程。
+            </Typography>
+        </PageHeader>
+        )
+    }
+
+function ScratchBase(){
+    return(
+        <PageHeader
+            title="Level 1"
+            subTitle="Scratch基础"
+            avatar={{src: level1logo}}
+        >
+            <div style={{
+                display:'flex',
+                justifyContent:'center',
+                flexDirection:'column',
+                alignItems:'center'
+            }}>
+                <img src={gameico} style={{height:'100px'}}/>
+            </div>            
+            <Typography>
+                    学习模块编程的基础，带您走进奇妙的编程世界。
+            </Typography>
+        </PageHeader>
+    )
 }
 
 export default HomePage;
